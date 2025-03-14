486a6179cb044921c7bbe4b47f4549ee2b29eacc
protitude <miles.france@gmail.com>
2025-03-14 11:24:18 -0600

    access_misc: d8-2347
    New view permission for registration on user page

---
+++ modules/access_misc/src/Plugin/views/access/ViewsUserRegistrationCustomAccess.php
@@ -0,0 +1,62 @@
+<?php
+
+
+namespace Drupal\access_misc\Plugin\views\access;
+
+use Drupal\views\Plugin\views\access\AccessPluginBase;
+use Drupal\Core\Session\AccountInterface;
+use Symfony\Component\Routing\Route;
+
+/**
+ * Class ViewsCustomAccess
+ *
+ * @ingroup views_access_plugins
+ *
+ * @ViewsAccess(
+ *     id = "views_user_registration_custom_access",
+ *     title = @Translation("Custom /user Registration Access"),
+ *     help = @Translation("Custom Registration Access for registration on the /user page."),
+ * )
+ */
+class ViewsUserRegistrationCustomAccess extends AccessPluginBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function summaryTitle() {
+    return $this->t('Custom User Registration Access');
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function access(AccountInterface $account) {
+    $access = FALSE;
+
+    // Get current uri.
+    $current_uri = \Drupal::service('path.current')->getPath();
+    $url_bits = explode('/', $current_uri);
+
+    $profile_uid = is_numeric($url_bits[2]) ? $url_bits[2] : 0;
+
+    $current_user_id = $account->id();
+
+    if ($profile_uid == $current_user_id) {
+      $access = TRUE;
+    }
+
+    if ($account->hasRole('administrator')) {
+      $access = TRUE;
+    }
+
+    return $access;
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alterRouteDefinition(Route $route) {
+    $route->setRequirement('_access', 'TRUE');
+  }
+}
