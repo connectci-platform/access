6f8d7278751910633457550407f8316e0588014a
Andrew Pasquale <andrew@elytra.net>
2025-03-19 10:47:09 -0400

    Update AffinityCoordinatorDocumentation.php
    New url for doc

--- modules/access_affinitygroup/src/Plugin/Block/AffinityCoordinatorDocumentation.php
+++ modules/access_affinitygroup/src/Plugin/Block/AffinityCoordinatorDocumentation.php
@@ -47,7 +47,7 @@ class AffinityCoordinatorDocumentation extends BlockBase {
         '#template' => '<a class="btn btn-outline-dark cursor-default mx-0 my-2" target="_blank" href="{{ link }}">{{ coordinator_text }}</a>',
         '#context' => [
           'coordinator_text' => $this->t('Coordinator Documentation'),
-          'link' => 'https://xsedetoaccess.ccs.uky.edu/confluence/redirect/Affinity+Group+Coordinator+Notes.html',
+          'link' => 'https://access-ci.atlassian.net/wiki/spaces/ACCESSdocumentation/pages/1263534105/Affinity+Group+Coordinator+Notes',
         ],
       ];
     }
