be8eff2456ee7313374d0bcc523ec54ed0c58aff
protitude <miles.france@gmail.com>
2025-06-12 10:18:26 -0600

    Adding in fixes from @a-pasquale d8-2333

--- modules/access_affinitygroup/src/Plugin/EmailWrapper.php
+++ modules/access_affinitygroup/src/Plugin/EmailWrapper.php
@@ -145,7 +145,7 @@ class EmailWrapper {
                   }
               }
 
-              .field--name-title, .field--name-recur-type {
+              .field--name-title, .field--name-recur-type, .field--name-event-instances .field__label, .field--name-field-affinity-group-node, .field--name-field-tags {
                 display: none;
               }
               .field { margin: 15px 0; }
