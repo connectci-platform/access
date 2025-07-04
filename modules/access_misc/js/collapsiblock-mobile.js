6d9ec76aad4980f4d7d71e4517c21eb384a4f690
protitude <miles.france@gmail.com>
2025-07-04 12:08:06 -0600

    misc: collapsiblock force collapse in cookie on small screens d8-2289

--- modules/access_misc/js/collapsiblock-mobile.js
+++ modules/access_misc/js/collapsiblock-mobile.js
@@ -1,11 +1,19 @@
 (function ($) {
   $(document).ready(function () {
-    // If window width is below 768px, make blocks collapsed
-    if ($(window).width() < 768) {
-      $('.collapsiblockTitle:not(.collapsiblockTitleCollapsed)').addClass('collapsiblockTitleCollapsed');
-      $('[aria-expanded="true]').attr('aria-expanded', 'false');
-      $('.collapsiblockContent:not(.collapsiblockContentCollapsed)').addClass('collapsiblockContentCollapsed');
-      $('.collapsiblockContent').attr('style', 'display: none;');
-    }
+    ((Drupal, cookies, once) => {
+      Drupal.CollapsiblockMisc = Drupal.CollapsiblockMisc || {};
+      const cookieString = cookies.get('collapsiblock');
+      const cookieData = cookieString ? JSON.parse(cookieString) : {};
+      // If window width is below 768px, make blocks collapsed
+      if ($(window).width() < 768) {
+        Object.keys(cookieData).forEach((key) => {
+          cookieData[key] = 0;
+        });
+        cookies.set(
+          'collapsiblock',
+          JSON.stringify(cookieData)
+        );
+      }
+    })(Drupal, window.Cookies, once);
   });
 })(jQuery);
