c55219ceb68bf952ba02398fe74aad1113a254ab
Andrew Pasquale <andrew@elytra.net>
2025-10-03 14:28:45 -0400

    Fix access and caching for Affinity Group page and View Members coordinators block

--- modules/access_affinitygroup/src/Plugin/views/access/ViewsCustomAccess.php
+++ modules/access_affinitygroup/src/Plugin/views/access/ViewsCustomAccess.php
@@ -38,13 +38,24 @@ class ViewsCustomAccess extends AccessPluginBase {
     if (in_array('administrator', $roles)) {
       $access = TRUE;
     }
+
+    // Try to get nid from query parameter first, then from route.
     $nid = \Drupal::request()->query->get('nid');
+    if (!$nid) {
+      $node = \Drupal::routeMatch()->getParameter('node');
+      if ($node) {
+        $nid = $node->id();
+      }
+    }
+
     if ($nid) {
       $node = \Drupal\node\Entity\Node::load($nid);
-      $coordinators = $node->get('field_coordinator')->getValue();
-      foreach ($coordinators as $coordinator) {
-        if ($coordinator['target_id'] == $account->id()) {
-          $access = TRUE;
+      if ($node) {
+        $coordinators = $node->get('field_coordinator')->getValue();
+        foreach ($coordinators as $coordinator) {
+          if ($coordinator['target_id'] == $account->id()) {
+            $access = TRUE;
+          }
         }
       }
     }
@@ -57,5 +68,37 @@ class ViewsCustomAccess extends AccessPluginBase {
    */
   public function alterRouteDefinition(Route $route) {
     $route->setRequirement('_access', 'TRUE');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheContexts() {
+    // Cache per user and per URL (since we check ?nid query parameter).
+    return ['user', 'url.query_args:nid'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheTags() {
+    $tags = parent::getCacheTags();
+
+    // Add the affinity group node as a cache tag so when it's updated
+    // (e.g., coordinators changed), the view cache is invalidated.
+    // Try query parameter first, then route.
+    $nid = \Drupal::request()->query->get('nid');
+    if (!$nid) {
+      $node = \Drupal::routeMatch()->getParameter('node');
+      if ($node) {
+        $nid = $node->id();
+      }
+    }
+
+    if ($nid) {
+      $tags[] = 'node:' . $nid;
+    }
+
+    return $tags;
   }
 }
