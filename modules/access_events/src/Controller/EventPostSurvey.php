4b0f6441c7238739cba2df99921958fb684f4a9c
protitude <miles.france@gmail.com>
2025-10-30 17:13:01 -0600

    access_events: d8-2577 Create PostSurvey method to send postsurvey email during cron. Added EventPostSurvey controller to mark survey complete and forward to url for survey.

---
+++ modules/access_events/src/Controller/EventPostSurvey.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Drupal\access_events\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Routing\RedirectDestinationInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Routing\TrustedRedirectResponse;
+
+/**
+ * Controller for forwarding post survey clicks.
+ */
+class EventPostSurvey extends ControllerBase {
+
+  /**
+   * Perform redirect.
+   *
+   * @var \Drupal\Core\Routing\RedirectDestinationInterface
+   */
+  protected $redirectDestination;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The database connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $database;
+
+  /**
+   * ID's of registrants.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $registrantIds;
+
+  /**
+   * The event instance id from uri.
+   *
+   * @var int
+   */
+  protected $eventInstanceId;
+
+  /**
+   * The event original url.
+   *
+   * @var string
+   */
+  protected $eventRegistrationUrl;
+
+  /**
+   * Constructs request stuff.
+   *
+   * @param \Drupal\Core\Routing\RedirectDestinationInterface $redirect_destination
+   *   The redirect destination service.
+   * @param \Drupal\Core\Database\Connection $database
+   *   The database connection.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(
+    RedirectDestinationInterface $redirect_destination,
+    Connection $database,
+    EntityTypeManagerInterface $entity_type_manager,
+  ) {
+    $this->redirectDestination = $redirect_destination;
+    $this->database = $database;
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container): self {
+    return new self(
+      $container->get('redirect.destination'),
+      $container->get('database'),
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
+   * Route to forward to post survey.
+   */
+  public function clicked(AccountInterface $user, Request $request) {
+    $entity_id = is_numeric($request->attributes->get('entity')) ? $request->attributes->get('entity') : 0;
+    $user_id = is_numeric($request->attributes->get('user')->id()) ? $request->attributes->get('user')->id() : 0;
+
+    $event_instance = $this->entityTypeManager->getStorage('eventinstance')->load($entity_id);
+    $entity_series = $event_instance->getEventSeries();
+    $post_survey_url = $entity_series->field_post_survey_url->uri;
+
+    // Check registration exists
+    $entity_query = \Drupal::entityQuery('registrant');
+    $entity_query->accessCheck(FALSE);
+    $entity_query->condition('eventinstance_id', $entity_id);
+    $entity_query->condition('user_id', $user_id);
+    $registrants = $entity_query->execute();
+
+    // Get id of registrant, there should only be one.
+    $registrant_id = reset($registrants);
+
+    if ($registrants) {
+      $registrant = \Drupal::entityTypeManager()->getStorage('registrant')->load($registrant_id);
+      $registrant->set('field_post_survey_reminder_sent', \Drupal::time()->getRequestTime());
+      $registrant->save();
+    }
+    else {
+      return [
+        '#type' => 'markup',
+        '#markup' => $this->t('You are not registered for this event.'),
+      ];
+    }
+
+    return new TrustedRedirectResponse($post_survey_url);
+  }
+
+}
---
+++ modules/access_events/src/Plugin/PostSurvey.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace Drupal\access_events\Plugin;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\Component\Datetime\TimeInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * Post Survey functions.
+ */
+class PostSurvey {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The time service.
+   *
+   * @var \Drupal\Component\Datetime\TimeInterface
+   */
+  protected $time;
+
+  /**
+   * The request stack.
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  protected $requestStack;
+
+  /**
+   * The site tools service.
+   *
+   * @var mixed
+   */
+  protected $siteTools;
+
+  /**
+   * The mail service.
+   *
+   * @var mixed
+   */
+  protected $mailService;
+
+  /**
+   * The logger factory.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelFactoryInterface
+   */
+  protected $loggerFactory;
+
+  /**
+   * Construct object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\Component\Datetime\TimeInterface $time
+   *   The time service.
+   * @param \Symfony\Component\HttpFoundation\RequestStack $request_stack
+   *   The request stack.
+   * @param mixed $site_tools
+   *   The site tools service.
+   * @param mixed $mail_service
+   *   The mail service.
+   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger_factory
+   *   The logger factory.
+   */
+  public function __construct(
+    EntityTypeManagerInterface $entity_type_manager,
+    TimeInterface $time,
+    RequestStack $request_stack,
+    $site_tools,
+    $mail_service,
+    LoggerChannelFactoryInterface $logger_factory,
+  ) {
+    $this->entityTypeManager = $entity_type_manager;
+    $this->time = $time;
+    $this->requestStack = $request_stack;
+    $this->siteTools = $site_tools;
+    $this->mailService = $mail_service;
+    $this->loggerFactory = $logger_factory;
+  }
+
+  /**
+   * Send post-survey email.
+   */
+  public function postSurveyEmail() {
+    // Entity query on 'entityseries' where 'field_pre_survey_url' is not empty.
+    $entity_query = $this->entityTypeManager->getStorage('eventinstance')->getQuery();
+    $entity_query->accessCheck(FALSE);
+    $entity_query->condition('field_post_survey_sent', 0);
+    $result = $entity_query->execute();
+
+    foreach ($result as $entity_id) {
+      $event_instance = $this->entityTypeManager->getStorage('eventinstance')->load($entity_id);
+
+      $end_date = $event_instance->date->end_value;
+      $end_date = strtotime($end_date);
+      $before_end = $end_date - (30 * 60);
+      $now = $this->time->getRequestTime();
+
+      if ($before_end <= $now) {
+        $policy = 'access_misc';
+
+        $entity_query = $this->entityTypeManager->getStorage('registrant')->getQuery();
+        $entity_query->accessCheck(FALSE);
+        $entity_query->condition('eventinstance_id', $entity_id);
+        $registrants = $entity_query->execute();
+
+        $series = $event_instance->getEventSeries();
+        $series_title = $series->get('title')->value;
+        $series_title_url = $this->siteTools->getEventCurrentDomainUrl($entity_id);
+        $series_post_survey_text = $series->get('field_post_survey_email_text')->value;
+
+        foreach ($registrants as $registrant_id) {
+          $registrant = $this->entityTypeManager->getStorage('registrant')->load($registrant_id);
+
+          if ($registrant->field_post_survey_sent->value == 1) {
+            continue;
+          }
+
+          $name = $registrant->field_first_name->value . ' ' . $registrant->field_last_name->value;
+          $email = $registrant->title->value;
+          $user_id = $registrant->user_id->target_id;
+          $domain = $this->requestStack->getCurrentRequest()->getHost();
+          $post_survey_url = "https://$domain/events/$entity_id/post_survey/$user_id";
+
+          // Get list of unique emails.
+          $variables = [
+            'title' => $series_title,
+            'name' => $name,
+            'title_link' => $series_title_url,
+            'post_survey_text' => $series_post_survey_text,
+            'post_survey_url' => $post_survey_url,
+          ];
+
+          $policy_subtype = 'post_survey';
+          try {
+            $this->mailService->email($policy, $policy_subtype, $email, $variables);
+          }
+          catch (\Exception $e) {
+            $this->loggerFactory->get('access_misc')
+              ->error('Error sending post survey email to ' . $email . ': ' . $e->getMessage());
+          }
+
+          // Mark Registrant as survey sent.
+          $registrant->set('field_post_survey_sent', $now);
+          $registrant->save();
+        }
+
+        // Mark Event Instance as survey sent.
+        $event_instance->field_post_survey_sent->value = 1;
+        $event_instance->save();
+      }
+    }
+
+  }
+
+}
