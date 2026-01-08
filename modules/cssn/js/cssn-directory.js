e8f2627d45d72e9a050c3281248006e88eeb1437
protitude <miles.france@gmail.com>
2026-01-08 11:59:43 -0700

    directory: d8-2621
    badges to list

--- modules/cssn/js/cssn-directory.js
+++ modules/cssn/js/cssn-directory.js
@@ -3,13 +3,13 @@ var item = document.querySelectorAll('.cssn-directory-item');
 item.forEach(addCount);
 function addCount(item) {
   // count .square-tag li inside index
-  var squareTags = item.querySelectorAll('li').length;
+  var squareTags = item.querySelectorAll('.square-tags li').length;
   if (squareTags > 5) {
     var squareTags = squareTags - 5;
     var more = "+ " + squareTags + " more";
-    item.querySelector('li:last-child').innerHTML = more;
-    item.querySelector('li:last-child').style.display = "flex";
-    item.querySelector('li:last-child').style.alignItems = "center";
-    item.querySelector('li:last-child').style.fontSize = "14px";
+    item.querySelector('.square-tags li:last-child').innerHTML = more;
+    item.querySelector('.square-tags li:last-child').style.display = "flex";
+    item.querySelector('.square-tags li:last-child').style.alignItems = "center";
+    item.querySelector('.square-tags li:last-child').style.fontSize = "14px";
   }
 }
