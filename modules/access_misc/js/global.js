74db355fe7bb417c675e887c93b2917ffa765324
protitude <miles.france@gmail.com>
2026-03-13 12:47:50 -0600

    misc: d8-2686
    global.js: Adding keyboard nav to checkboxes

--- modules/access_misc/js/global.js
+++ modules/access_misc/js/global.js
@@ -1,5 +1,71 @@
-(function ($, Drupal) {
+// Checbox list behavior for accessibility and keyboard navigation,
+// and a heading for filters on mobile.
+(function ($, Drupal, once) {
   'use strict';
+
+  Drupal.behaviors.accessMiscCheckboxList = {
+    attach: function (context, settings) {
+      once('access-misc-checkbox-list', 'ul.item-list__checkbox', context).forEach(function (list) {
+        list.querySelectorAll('li').forEach(function (item, index) {
+          item.setAttribute('aria-checked', 'false');
+          item.setAttribute('tabindex', '-1');
+          item.setAttribute('aria-selected', index === 0 ? 'true' : 'false');
+        });
+
+        list.querySelectorAll('[type=checkbox]').forEach(function (checkbox) {
+          checkbox.addEventListener('click', function (e) {
+            if (this.checked) {
+              this.parentElement.parentElement.setAttribute('aria-checked', 'true');
+            } else {
+              this.parentElement.parentElement.setAttribute('aria-checked', 'false');
+            }
+            e.stopPropagation();
+          });
+        });
+
+        list.addEventListener('keydown', function (e) {
+          var currentItem = this.querySelector('[aria-selected=true]');
+          if (!currentItem) return;
+          switch (e.keyCode) {
+            case 38: // Up arrow
+              if (currentItem.previousElementSibling !== null) {
+                currentItem.setAttribute('aria-selected', 'false');
+                currentItem.classList.remove('active');
+                currentItem.previousElementSibling.setAttribute('aria-selected', 'true');
+                currentItem.previousElementSibling.classList.add('active');
+                currentItem.previousElementSibling.focus();
+              }
+              e.preventDefault();
+              break;
+            case 40: // Down arrow
+              if (currentItem.nextElementSibling !== null) {
+                currentItem.setAttribute('aria-selected', 'false');
+                currentItem.classList.remove('active');
+                currentItem.nextElementSibling.setAttribute('aria-selected', 'true');
+                currentItem.nextElementSibling.classList.add('active');
+                currentItem.nextElementSibling.focus();
+              }
+              e.preventDefault();
+              break;
+            case 32: // Space
+              var checkbox = currentItem.querySelector('input[type=checkbox]');
+              if (checkbox) {
+                if (currentItem.getAttribute('aria-checked') === 'true') {
+                  currentItem.setAttribute('aria-checked', 'false');
+                  checkbox.checked = false;
+                } else {
+                  currentItem.setAttribute('aria-checked', 'true');
+                  checkbox.checked = true;
+                }
+                $(checkbox).trigger('change');
+              }
+              e.preventDefault();
+              break;
+          }
+        });
+      });
+    }
+  };
 
   Drupal.behaviors.accessMiscFiltersHeading = {
     attach: function (context, settings) {
@@ -11,4 +77,4 @@
     }
   };
 
-})(jQuery, Drupal);
+})(jQuery, Drupal, once);
