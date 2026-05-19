52e1eb40f374d65939ca8cab88b2c9b5cadf4677
Andrew Pasquale <andrew@elytra.net>
2026-05-19 18:36:27 -0400

    chore: update access-ci-ui to 0.20.5

--- js/headerfooter.js
+++ js/headerfooter.js
@@ -10,7 +10,7 @@ import {
   qaBot,
   siteMenus,
   universalMenus,
-} from "https://unpkg.com/@access-ci/ui@0.20.2/dist/access-ci-ui.js";
+} from "https://unpkg.com/@access-ci/ui@0.20.5/dist/access-ci-ui.js";
 
 (function (Drupal, drupalSettings) {
 
