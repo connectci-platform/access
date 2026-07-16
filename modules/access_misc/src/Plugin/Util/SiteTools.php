cf20d2430d5409ee8733f6060297c04a584d5370
protitude <miles.france@gmail.com>
2026-07-16 11:58:34 -0400

    affinitygroup/mis/news: d8-2778
    Fix domain urls

--- modules/access_misc/src/Plugin/Util/SiteTools.php
+++ modules/access_misc/src/Plugin/Util/SiteTools.php
@@ -4,6 +4,7 @@ namespace Drupal\access_misc\Plugin\Util;
 
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Component\Utility\Html;
+use Drupal\Component\Utility\UrlHelper;
 use Drupal\Core\Utility\Token;
 
 /**
@@ -154,6 +155,12 @@ class SiteTools {
    *   The absolute URL.
    */
   public function buildDomainAwareUrl($relative_path, $entity = NULL) {
+    // Under Domain Source, a canonical URL passed in may already be an
+    // absolute URL. Return it unchanged so the domain host is not prepended
+    // a second time (e.g. https://ccmnet.orghttps://ccmnet.org/...).
+    if (UrlHelper::isExternal($relative_path)) {
+      return $relative_path;
+    }
     try {
       // Try to get domain from entity's domain_access field.
       if ($entity && $entity->hasField('domain_access')) {
