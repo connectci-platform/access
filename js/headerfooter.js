b152305ed22e09cc954ca0c373308103db30db81
Andrew Pasquale <andrew@elytra.net>
2026-07-27 22:25:14 -0500

    chore: bump @access-ci/ui to 0.20.11 in header/footer loader

--- js/headerfooter.js
+++ js/headerfooter.js
@@ -10,7 +10,7 @@ import {
   qaBot,
   siteMenus,
   universalMenus,
-} from "https://unpkg.com/@access-ci/ui@0.20.10/dist/access-ci-ui.js";
+} from "https://unpkg.com/@access-ci/ui@0.20.11/dist/access-ci-ui.js";
 
 (function (Drupal, drupalSettings) {
 
